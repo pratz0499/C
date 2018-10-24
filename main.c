827c9a19ddfc1d94e1491652d7b38b32bf416ebb
pratz0499 <36686740+pratz0499@users.noreply.github.com>
2018-10-24 16:47:56 +0530

    Update main.c
    have to edit push and pop

--- main.c
+++ main.c
@@ -2,43 +2,55 @@
 struct stack
 {
     int top;
-    int data[100];
     int size;
+    void *data[100];
 };
 
 void push(struct stack *p, void *item)
 {
     if(p->top==p->size-1)
     {
-        printf("the stack is full");
+       printf("the stack is full");
     }
     else
     {
-        p->top=p->top+1;
-        p->data[p->top]=p->data[p->top]->item;
+        p->top++;
+        p->data[p->top]=item;
     }
 
 }
-void *pop(struct stack *p)
+void pop(struct stack *p)
 {
-    if(p.top=-1)
+    if(p->top=-1)
     {
         printf("the stack is empty");
     }
     else
     {
-        p.top=p.top-1;
+        p->top=p->top-1;
+        return void *p->data[top];
+    }
+
+void display(struct stack p)
+{
+    int i;
+    for(i=p.top;i>=0;i--)
+    {
+        printf("%d",*(int*)p.data[i]);
     }
 }
 
-void main()
+
+
+
+int main()
 {
-    struct stack t ={100,10};
+
     int item;
-    void *retitem;
-    printf("enter the item");
-    scanf("%d",&item);
-    push(&t,item)
-    retitem = pop(&t);
-    printf("%d",*(int *)retitem)
+    struct stack p={-1,100};
+    printf("Enter the item: ");
+    scanf("%d", &item);
+    push(&p, &item);
+    display(p);
+    return 0;
 }
