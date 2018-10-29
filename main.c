3f0695c7b3951e3939796ed379711ee0b75c02ec
pratz0499 <36686740+pratz0499@users.noreply.github.com>
2018-10-29 15:50:54 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 struct stack
 {
     int top;
@@ -33,20 +33,39 @@ void *pop(struct stack *s, int *error)
         return temp;
     }
 }
- void display(struct stack s,void *item)
+ void display_int(struct stack s)
  {
-    int i ;
+   int i;
    for(i=s.top;i>=0;i--)
    {
-        printf("%d",*(char*)s->data[i]);
+        printf("%p",*(char*)s.data[i]);
 
    }
 
  }
+ void display_float(struct stack f)
+ {
+     float j;
+     for(j=f.top;j>0;j--)
+     {
+          printf("%p",*(char*)f.data[j]);
+     }
+ }
+ void display_str(struct stack c)
+ {
+     char k;
+     for(k=c.top;k>0;k--)
+     {
+         printf("%p",*(char*)c.data[k]);
+     }
+ }
+
 int main()
 {
     int item ,error=0;
     struct stack s={-1,100};
+    struct stack f={-1,100};
+    struct stack c={-1,100};
 
     printf("Enter the element: ");
     scanf("%d",&item);
@@ -56,9 +75,12 @@ int main()
     {
         printf("Stack Empty");
     }
-    else{
-        display(s);
-    }
+    else
+        {
+        display_int(s);
+        display_float(f);
+        display_str(c);
+        }
 
 
 }
